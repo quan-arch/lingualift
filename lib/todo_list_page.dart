5d31d640e737ee74d929cafeb4ecc8a8a1cb9d4f
Quan Tran <quan.th@mission.plus>
2025-03-05 15:50:07 +0700

    add page view

--- lib/todo_list_page.dart
+++ lib/todo_list_page.dart
@@ -12,9 +12,14 @@ import 'package:lingualift/widgets/todos/todo_page_two.dart';
 
 import 'widgets/todos/todo_page_one.dart';
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
 
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,14 +58,21 @@ class TodoListPage extends StatelessWidget {
                     SizedBox(height: 68),
                     _buildCheckedBoxGroup(context),
                     SizedBox(height: 56),
-                    Text('TO DO LIST', style: GoogleFonts.quicksand(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.black,
-                      height: 1.25,
-                    ),),
+                    Text(
+                      'TO DO LIST',
+                      style: GoogleFonts.quicksand(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.black,
+                        height: 1.25,
+                      ),
+                    ),
                     SizedBox(height: 62),
-                    TodoPageTwo(),
+                    Flexible(child: _buildPageView(context)),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: _buildPageIndicator(),
+                    )
                   ],
                 ),
               ),
@@ -79,7 +91,9 @@ class TodoListPage extends StatelessWidget {
         child: ListView.separated(
             scrollDirection: Axis.horizontal,
             itemBuilder: (BuildContext context, int index) {
-              return TodoCheckedBoxItem(todoEntity: todos[index],);
+              return TodoCheckedBoxItem(
+                todoEntity: todos[index],
+              );
             },
             separatorBuilder: (BuildContext context, int index) {
               return SizedBox(width: 18);
@@ -89,4 +103,55 @@ class TodoListPage extends StatelessWidget {
     );
   }
 
+  PageController _pageController = PageController();
+
+  Widget _buildPageView(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      onPageChanged: (int page) {
+        setState(() {
+          selectedIndex = page;
+        });
+      },
+      itemCount: 3,
+      itemBuilder: (BuildContext context, int index) {
+        switch(index){
+          case 0:
+            return TodoPageOne();
+          case 1:
+            return TodoPageTwo();
+          case 2:
+            return TodoPageThree();
+          default:
+            return TodoPageOne();
+        }
+      },
+    );
+  }
+
+  int selectedIndex = 0;
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < 3; i++) {
+      list.add(i == selectedIndex ? _indicator(true) : _indicator(false));
+    }
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return SizedBox(
+      height: 10,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 150),
+        margin: EdgeInsets.symmetric(horizontal: 4.0),
+        height: isActive ? 10 : 8.0,
+        width: isActive ? 12 : 8.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isActive ? AppColors.blue : AppColors.grey,
+        ),
+      ),
+    );
+  }
 }
